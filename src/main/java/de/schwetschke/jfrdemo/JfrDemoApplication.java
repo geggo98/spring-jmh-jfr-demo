8168d1271393da712f2d065c1606422e56664eba
Stefan Schwetschke <stefan@schwetschke.de>
2019-03-13 20:40:14 +0100

    Add inital project

---
+++ src/main/java/de/schwetschke/jfrdemo/JfrDemoApplication.java
@@ -0,0 +1,13 @@
+package de.schwetschke.jfrdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfrDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfrDemoApplication.class, args);
+	}
+
+}
